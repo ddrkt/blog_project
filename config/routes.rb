723ad311adc8db0ba3852f2ebb22f12bc30d4c1f
ddrkt <toxa.ship@gmail.com>
2019-12-27 05:02:24 +0300

    feat(comments): now you can delete comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   use_doorkeeper
   resources :posts do
-    resources :comments
+    resources :comments, shallow: true, only: [:create, :update, :destroy]
   end
 
   root to: "posts#index"
